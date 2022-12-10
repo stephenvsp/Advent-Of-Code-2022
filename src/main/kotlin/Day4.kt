1bbaf9fbc505c3e50f21d9b972890a8ee5020122
stephenvsp <stephenvsp@gmail.com>
2022-12-10 13:48:05 -0500

    day 4 part 2

--- src/main/kotlin/Day4.kt
+++ src/main/kotlin/Day4.kt
@@ -10,23 +10,33 @@ fun day4part1(): Int {
     lines.forEach() {
         val (lower1, upper1, lower2, upper2) = Regex("^(\\d+)-(\\d+),(\\d+)-(\\d+)$").find(it)!!.destructured
 
-        if (contains((lower1.toInt()..upper1.toInt()).toSet(), (lower2.toInt()..upper2.toInt()).toSet())) count++
+        val range1 = (lower1.toInt()..upper1.toInt()).toSet()
+        val range2 = (lower2.toInt()..upper2.toInt()).toSet()
+        if (range1.containsAll(range2) || range2.containsAll(range1)) count++
     }
 
     return count
 }
 
-fun contains(range1: Set<Int>, range2: Set<Int>): Boolean {
-
-    if (range1.containsAll(range2)) return true
-    if (range2.containsAll(range1)) return true
-
-    return false
-}
 
 fun day4part2(): Int {
-    return 0
+    val lines = File("src/main/resources/input4.txt").readLines()
+
+    var count = 0
+
+    lines.forEach() {
+        val (lower1, upper1, lower2, upper2) = Regex("^(\\d+)-(\\d+),(\\d+)-(\\d+)$").find(it)!!.destructured
+
+        val range1 = (lower1.toInt()..upper1.toInt()).toSet()
+        val range2 = (lower2.toInt()..upper2.toInt()).toSet()
+
+        if (range1.intersect(range2).isNotEmpty()) count++
+    }
+
+    return count
 }
+
+
 
 fun main() {
     println("Day 4")
