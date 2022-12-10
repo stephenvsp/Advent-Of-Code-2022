16900e0bcd6a5a174eb722ef565d7f9e226be84e
stephenvsp <stephenvsp@gmail.com>
2022-12-10 00:20:18 -0500

    refactor out class

--- src/main/kotlin/Day3.kt
+++ src/main/kotlin/Day3.kt
@@ -2,41 +2,39 @@ package main.kotlin
 
 import java.io.File
 
-class Day3 {
+fun part1(): Int {
+    val lines = File("src/main/resources/input3.txt").readLines()
 
-    fun part1(): Int {
-        val lines = File("src/main/resources/input3.txt").readLines()
+    return lines.sumOf {
+        val mid = it.length / 2
+        val first = it.substring(0, mid)
+        val second = it.substring(mid, it.length)
 
-        return lines.sumOf {
-            val mid = it.length / 2
-            val first = it.substring(0, mid)
-            val second = it.substring(mid, it.length)
+        val shared = first.toSet().intersect(second.toSet()).first()
 
-            val shared = first.toSet().intersect(second.toSet()).first()
+        val code = shared.code
 
-            val code = shared.code
-
-            if (shared.isUpperCase()) {
-                code - 38
-            }
-            else {
-                code - 96
-            }
+        if (shared.isUpperCase()) {
+            code - 38
+        } else {
+            code - 96
         }
     }
+}
 
-    fun part2(): Int {
-        val lines = File("src/main/resources/input3.txt").readLines()
+fun part2(): Int {
+    val lines = File("src/main/resources/input3.txt").readLines()
 
-        return lines.chunked(3).sumOf{ (a, b, c) ->
+    return lines.chunked(3).sumOf { (a, b, c) ->
 
-            val shared = a.toSet().intersect(b.toSet().intersect(c.toSet())).first()
+        val shared = a.toSet().intersect(b.toSet().intersect(c.toSet())).first()
 
-            if (shared.isUpperCase()) shared - 'A' + 27 else shared - 'a' + 1
-
-        }
-
-
-
+        if (shared.isUpperCase()) shared - 'A' + 27 else shared - 'a' + 1
     }
+}
+
+fun main() {
+    println("Day 3")
+    println("Part 1: ${part1()}")
+    println("Part 2: ${part2()}")
 }
